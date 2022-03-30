56544ed0a1dd008d9630f177fb5591a11479afdc
hnsoo <khsoo2439@gmail.com>
2022-03-30 18:36:23 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,33 @@
+// Express 없이 기본 서버 구동
+// const http = require('http');
+// const app = http.createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8"});
+//     console.log(req.url);
+//     if (req.url == '/') {
+//         res.end('여기는 루트 입니다.');
+//     } else if (req.url == '/login') {
+//         res.end('여기는 로그인 입니다.');
+//     }
+// });
+
+// app.listen(3001, ()=>{
+//     console.log('http로 가동된 서버입니다.');
+// });
+"use strict";
+
+// 모듈
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// 라우팅
+const home = require("./src/routes/home");
+
+// 앱 세팅
+app.set('views', path.join(__dirname, 'views')); 
+app.set("view engine", "ejs");
+
+app.use("/", home);  // use -> 미들웨어
+
+module.exports = app;
+
