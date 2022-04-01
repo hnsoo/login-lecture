f7e627120cc4e28ffa8eed34e8e3cdb3949d6a22
hnsoo <khsoo2439@gmail.com>
2022-04-01 19:25:08 +0900

    UserStorage 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,7 @@
 "use strict"
 
+const UserStorage = require('../../models/UserStorage')
+
 const output = {
     hello: (req, res) => {
         res.render("home/index");
@@ -9,29 +11,25 @@ const output = {
     }
 }
 
-const users = {
-    id: ["aaa", "bbb", "ccc"],
-    psword: ["1234", "1234", "123456"]
-}
-
 const process = {
     login: (req, res) => {
         const id = req.body.id,
          psword = req.body.psword;
 
+        const users = UserStorage.getUsers('id', 'psword', 'name');
+        console.log(users);
+        const response = {}
         if (users.id.includes(id)) {
             const idx = users.id.indexOf(id);
             if (users.psword[idx] === psword) {
-                return res.json({
-                    success: true
-                })
+                response.success = true;
+                return res.json(response)
             }
         }
 
-        return res.json({
-            sucess: false,
-            msg: '로그인에 실패했습니다.'
-        })
+        response.success = false;
+        response.msg = "로그인에 실패했습니다.";
+        return res.json(response)
     }
 }
 
